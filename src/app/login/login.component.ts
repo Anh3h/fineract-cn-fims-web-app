657ba54c26715fddfaca0dd5948574668a4f1e98
Anh3h <courageangeh@gmail.com>
2019-10-17 16:39:14 +0100

    Re-design login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,7 +30,91 @@ import {MatSelectChange} from '@angular/material';
 
 @Component({
   selector: 'fims-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [
+    `
+      .container-fluid {
+        background-image: url('./assets/images/bg-1.jpg');
+        background-position: center top;
+      }
+      .login-form {
+        width: 430px;
+        margin: 0 auto;
+      }
+      img {
+        width: 312px;
+        height: 51px;
+        display: block;
+        margin: 0 auto 3rem auto;
+      }
+      @media (max-width: 575.98px) {
+        img {
+          width: 255px;
+          height: 51px;
+        }
+      }
+      h3.title {
+        color: #fff;
+        text-align: center;
+        font-size: 1.5rem;
+        font-weight: 500;
+      }
+      .login-form {
+        padding: 6% 1rem 1rem 0rem;
+      }
+      form {
+        margin: 4rem auto;
+      }
+      input {
+        border-radius: 46px;
+        border: none;
+        background: rgba(67, 34, 167, 0.4);
+        color: #fff;
+        height: 46px;  
+        padding-left: 1.5rem;
+        padding-right: 1.5rem;
+        margin-top: 1.5rem;
+      }
+      input:focus {
+        color: #fff;
+        background: rgba(67, 34, 167, 0.4);
+        border: none;
+      }
+      small {
+        color: #646c9a;
+        font-weight: 550;
+        padding-left: 1.5rem;
+      }
+      ::placeholder {
+        color: #FFF;
+        opacity: 0.7;
+        font-size: 14px;
+        font-weight: 550;
+      }
+      .form-action {
+        text-align: center;
+      }
+      button[type=submit] {
+        height: 46px;
+        display: inline-block;
+        text-align: center;
+        padding-left: 3rem;
+        padding-right: 3rem;
+        margin-top: 0.8rem;
+        border-radius: 60px;
+        background: transparent;
+        color: rgba(255, 255, 255, 0.8);
+        border-color: rgba(255, 255, 255, 0.4);
+      }
+      .security-msg {
+        margin-top: 5rem;
+        font-size: 1rem;
+        font-weight: 500;
+        color: #bbabf1;
+        text-align: center;
+      }
+    `
+  ]
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
@@ -89,7 +173,7 @@ export class LoginComponent implements OnInit, OnDestroy {
     const tenant = this.form.get('tenant').value;
     const username = this.form.get('username').value;
     const password = this.form.get('password').value;
-
+    console.log(tenant, username, password)
     this.store.dispatch({
       type: LOGIN, payload: {
         username,
